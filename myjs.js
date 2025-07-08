e4bf03ad59ba67ea0c8eb283c5849ebd1e198a26
tran8215 <tranthithuytai.2003@gmail.com>
2025-07-08 09:50:50 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,6 +1,6 @@
 alert('Hello, Osamu!');
 alert(1+1);
 alert(Math.sqrt(1+1));
-sum = Math.sqrt(2) + Math.sqrt(3);
-result = 1 / 0;
-result = 0 / 0;
+let sum = Math.sqrt(2) + Math.sqrt(3);
+console.log(２の平方根と３の平方根の和:", sum);
+
